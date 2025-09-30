b6a1161606fe26035dfb74a8a2620404da2be6e5
Your Name <aydenait2025@gmail.com>
2025-09-29 22:53:09 -0400

    Add id='advantage' to competitive moat section for smooth scrolling

--- src/components/sections/market.tsx
+++ src/components/sections/market.tsx
@@ -197,6 +197,7 @@ export function Market() {
 
         {/* Competitive Moat */}
         <motion.div
+          id="advantage"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.5 }}
